7cf30f9f9d22918cbfb4ee95b02d3d0d74fc4f7f
Esteban Chavarria <gsonhis@gmail.com>
2026-06-11 16:55:41 -0600

    Agregar Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Nutrición Efectiva | Nutrición personalizada",
@@ -27,7 +28,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
     </html>
   );
 }
